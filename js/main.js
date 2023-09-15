1de53c1158d0bb328abfca56307fa5116fa7bd71
Francisca <franpontiggia@gmail.com>
2023-09-15 16:14:59 -0300

    codigo para el formulario con emailjs

--- js/main.js
+++ js/main.js
@@ -1,36 +1,27 @@
-// Agrega el evento para resaltar el párrafo cuando se hace clic en el enlace interno
 document.addEventListener("DOMContentLoaded", function () {
-    const quienesSomosLink = document.querySelector('a[href="#quienes_somos"]');
-    const parrafoQuienesSomos = document.getElementById('parrafo-quienes-somos');
-  
-    quienesSomosLink.addEventListener('click', function (event) {
-      event.preventDefault(); // Evita el comportamiento predeterminado del enlace interno
-      parrafoQuienesSomos.classList.toggle('highlighted'); // Alterna la clase 'highlighted' en el párrafo
-      setTimeout(() => {
-        parrafoQuienesSomos.classList.toggle('highlighted'); // Quita la clase 'highlighted' después de 1 segundo
-      }, 1000);
-    });
+  const quienesSomosLink = document.querySelector('a[href="#quienes_somos"]');
+  const parrafoQuienesSomos = document.getElementById('parrafo-quienes-somos');
+
+  quienesSomosLink.addEventListener('click', function (event) {
+    event.preventDefault();
+    parrafoQuienesSomos.classList.toggle('highlighted');
+    setTimeout(() => {
+      parrafoQuienesSomos.classList.toggle('highlighted');
+    }, 1000);
   });
-  
-  
-  
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-  
-  
-    
+});
 
-
+window.onload = function() {
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+      event.preventDefault();
+      // Genera un número aleatorio de cinco dígitos para la variable 'contact_number'
+      this.contact_number.value = Math.random() * 100000 | 0;
+      // Estos IDs deben coincidir con los valores que configuraste en EmailJS
+      emailjs.sendForm('service_5vnd5v9', 'template_ea6raia', this)
+          .then(function() {
+              console.log('SUCCESS!');
+          }, function(error) {
+              console.log('FAILED...', error);
+          });
+  });
+}
